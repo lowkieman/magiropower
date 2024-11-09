21ae914450453b4607aef93d2b9e9adc99a3324b
lowkieman <lowkieman@gmail.com>
2024-11-09 05:11:38 +0300

    wow it looks good in react

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import "./App.css"
 
 function App() {
-  const [count, setCount] = useState(0)
+  //showing navigation bar
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header>
+          <div class="header">
+              <i class="bi-list" id="bi-list"></i>
+              <a href="#"><img src="https://tinyurl.com/37vk7jzm" alt="logo"/></a>
+          </div>
+          <div class="nav" id="nav">
+              <i class="bi-x" id="bi-x"></i>
+              <ul>
+                  <li>Home</li>
+                  <li id="about1">About</li>
+                  <li id="power1">Power</li>
+                  <li id="services1">Services</li>
+                  <li id="partners1">Partners</li>
+                  <li id="media">Media</li>
+              </ul>
+          </div>
+        </header>
+        <main>
+            <div className="home">
+                <img src="https://tinyurl.com/bdcwpzjr" alt=""/>
+            </div>
+            <div className="about">
+                <img src="https://tinyurl.com/6byexdaw" alt=""/>
+            </div>
+            <div className="services">
+                <img src="https://tinyurl.com/6byexdaw" alt=""/>
+            </div>
+            <div className="partners">
+                <img src="https://tinyurl.com/6byexdaw" alt=""/>
+            </div>
+            <div className="blog">
+                <img src="https://tinyurl.com/6byexdaw" alt=""/>
+            </div>
+            <div className=""></div>
+            
+        </main>
+        <footer>
+            <img src="https://tinyurl.com/275njx8z"  alt=""/>
+        </footer>
     </>
   )
 }
